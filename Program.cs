9d3ed58b83131fbfa8ec0659acb274056326f316
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-25 22:36:59 +0530

    added UC-4 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace AddressBook
             Console.WriteLine("Use Case #1: Create a contact in Address Book");
             Console.WriteLine("Use Case #2: Add a new contact in Address Book");
             Console.WriteLine("Use Case #3: Edit existing contact using name in Address Book");
+            Console.WriteLine("Use Case #4: Delete a contact using name in Address Book");
 
             Console.Write("Choose a use case number to run the program: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -32,6 +33,13 @@ namespace AddressBook
                     addressBookThree.Display();
                     addressBookThree.EditContact();
                     addressBookThree.Display();
+                    break;
+                case 4:
+                    AddressBook addressBookFour = new AddressBook();
+                    addressBookFour.AddContact();
+                    addressBookFour.Display();
+                    addressBookFour.DeleteContact();
+                    addressBookFour.Display();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid use case number!");
