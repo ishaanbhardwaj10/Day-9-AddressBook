67352f4fbc23d9cc308f337f7a44a23da824855b
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-25 21:33:17 +0530

    added a parameterless constructor and Add method

--- AddressBook.cs
+++ AddressBook.cs
@@ -11,6 +11,10 @@ namespace AddressBook
 
         private Contact[] addressBook;
 
+        public AddressBook()
+        {
+            this.addressBook = new Contact[1];
+        }
 
         public AddressBook(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
         {
@@ -19,6 +23,37 @@ namespace AddressBook
             addressBook[0] = contact;
         }
 
+        public void AddContact()
+        {
+            Console.WriteLine("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter second name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter address: ");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter city: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter state: ");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter zip: ");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Enter phone number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter email: ");
+            string email = Console.ReadLine();
+
+            Contact contact = new Contact();
+            contact.FirstName = firstName;
+            contact.LastName = lastName;
+            contact.Address = address;
+            contact.City = city;
+            contact.State = state;
+            contact.Zip = zip;
+            contact.PhoneNumber = phoneNumber;
+            contact.Email = email;
+
+            addressBook[0] = contact;
+        }
 
         public void Display()
         {
