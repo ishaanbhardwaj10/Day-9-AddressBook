a10c8ec17673eb144e80f735e8c7337c5741a41c
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-25 21:32:35 +0530

    added UC-2 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AddressBook
+﻿using System;
+
+namespace AddressBook
 {
     class Program
     {
@@ -7,6 +9,7 @@
 
             Console.WriteLine("Welcome to Address Book Program");
             Console.WriteLine("Use Case #1: Create a contact in Address Book");
+            Console.WriteLine("Use Case #2: Add a new contact in Address Book");
 
             Console.Write("Choose a use case number to run the program: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -16,6 +19,11 @@
                 case 1:
                     AddressBook addressBookOne = new AddressBook("John", "Doe", "House-100", "Texas", "Florida", "111111", "9876543210", "john@google.com");
                     addressBookOne.Display();
+                    break;
+                case 2:
+                    AddressBook addressBookTwo = new AddressBook();
+                    addressBookTwo.AddContact();
+                    addressBookTwo.Display();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid use case number!");
